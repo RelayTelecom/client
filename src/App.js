8274ca1d6fe1c8c443cef7f31cb837c265b2bdee
Steven Ireland <scientastersteve@gmail.com>
2017-06-03 08:43:50 -0400

    Basic routing and connection to socket io

--- src/App.js
+++ src/App.js
@@ -1,52 +1,26 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
-import {Input, Button} from 'semantic-ui-react';
-import getUserMedia from 'getusermedia';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import Call from './Call';
+import NotFound from './NotFound';
 
 class App extends Component {
-  componentDidMount() {
-    //const socket = io("http://localhost:8642");
-
-    getUserMedia({audio: true}, (err, stream) => {
-      if (err) {
-        console.log(err);
-        console.log("Whoops!");
-      } else {
-        console.log("yay!");
-        const context = new AudioContext();
-        const source = context.createMediaStreamSource(stream);
-        const analyser = context.createScriptProcessor(1024,1,1);
-
-        analyser.onaudioprocess = (audio) => {
-          //socket.emit('audioBuffer', audio.outputBuffer);
-        };
-
-        analyser.connect(context.destination);
-
-        console.log(analyser);
-        this.refs.audio.src = window.URL.createObjectURL(source.mediaStream);
-        console.log(stream);
-      }
-    });
-  }
-
   render() {
     return (
-      <div className="App">
-        <div className="content">
-          <div className = "logoContainer">
-            <h1>Relay</h1>
-          </div>
-          <div className="callContainer">
-            <Input size="huge" placeholder="Address"></Input>
-            <Button inverted size="huge" color="green" >Call</Button>
-          </div>
-        </div>
-        <audio ref="audio" autoPlay></audio>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/call/:address/" component={Call} />
+          <Route path="/call/:address/:encryptionKey" component={Call} />
+          <Route path="/call/:address/:encryptionKey/:relay" component={Call} />
+
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     );
   }
+
 }
 
 export default App;
